6936a928a9c71678b02801b843691622b9145207
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2024-02-01 19:41:27 -0300

    Implementada a abstração.

--- src/Default/MSNMenssenger.java
+++ src/Default/MSNMenssenger.java
@@ -1,5 +1,12 @@
 package Default;
 
 public class MSNMenssenger extends ServicoMensagemInstantanea {
+        public void enviarMensagem() {
+            System.out.println("Enviando mensagem pelo MSN Messenger");
+        }
+        public void receberMensagem() {
+            System.out.println("Recebendo mensagem pelo MSN Messenger");
+        }
+    }
 
-}
+
